b7096c533508e90b4a36645db65e01c2acb541e6
aulianisa21 <76479927+aulianisa21@users.noreply.github.com>
2021-09-19 13:41:37 +0700

    Update Aulia Nisa Agus Setiana_Final_Project.py

--- Aulia Nisa Agus Setiana_Final_Project.py
+++ Aulia Nisa Agus Setiana_Final_Project.py
@@ -1,20 +1,37 @@
 from email import message
-from os import read
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 import smtplib
+import getpass
 
-msg = MIMEMultipart()
-msg['FROM'] = input('Masukkan Email Anda = ') #diisi email Anda (pengirim)
-msg['TO'] = open("D:\Dokumen\AI For Everyone, AI For Indonesia/receiver_list.txt", "r") ##Diisi email tujuan (penerima)
-msg['Subject'] = ('Lets introduce my self!') #Subject Email
-body = open("D:\Dokumen\AI For Everyone, AI For Indonesia/message.txt", "r") #isian email yang terdapat pada file message.txt
-pwd = input("Masukan Password Anda = ") #diisi password Anda (pengirim)
-server = smtplib.SMTP('smtp.gmail.com:587')
-server.starttls()
-server.login(msg['FROM'], pwd)
-server.sendmail(msg['FROM'], msg['TO'], msg.as_string)
-server.quit()
+alamat_email = []                              
+i = 1                                        
+
+def sent_email ():
+    fromaddr = pengirim
+    toaddr = open ('receive_list.txt', 'r')
+    print(toaddr.read())
+    msg = MIMEMultipart()
+    msg['FROM'] = fromaddr                       
+    msg['TO'] = toaddr                      
+    msg['SUBJECT'] = subject_email  
+    msg.attach(MIMEText(body_email, 'plain'))             
+ 
+pengirim = input('Masukkan E-mail Anda = ')     
+penerima = open("D:\Dokumen\AI For Everyone, AI For Indonesia/receiver_list.txt", "r")
+print(penerima.read())                 
+password_email = getpass.getpass('Masukkan Password Anda = ')     
+subject_email = input('Masukkan Subject = ')                         
+body_email = open("D:\Dokumen\AI For Everyone, AI For Indonesia/message.txt", "r")
+print(body_email.read()) 
+
+server = smtplib.SMTP('smtp.gmail.com', 587) 
+server.starttls()                              
+server.login(pengirim, password_email)                           
+server.sendmail(pengirim, [penerima], body_email)     
+server.quit()                                       
+        
 
 '''https://rifqifai.com/program-python-untuk-mengirim-email/
 https://code.tutsplus.com/id/tutorials/sending-emails-in-python-with-smtp--cms-29975'''
+''' https://www.pythonindo.com/cara-mengirim-email-menggunakan-python'''
